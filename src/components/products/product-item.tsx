e970035151500f522166e61c630ca9493fe2440f
IgorST <igorstojadinovic.vs@gmail.com>
2025-10-02 17:53:23 +0200

    Image optimization: - When with getIgameProps approach here instead of  next Image

--- src/components/products/product-item.tsx
+++ src/components/products/product-item.tsx
@@ -2,7 +2,7 @@ import Button from "../button";
 import { type ProductItem } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-
+import { getImageProps } from "next/image";
 export default function ProductItem({
     product,
     className,
@@ -17,21 +17,53 @@ export default function ProductItem({
                 className
             )}
         >
-            <picture className="lg:w-1/2 lg:rounded-lg">
-                <source
-                    media="(min-width: 1024px)"
-                    srcSet={product.images.desktop}
-                />
-                <source
-                    media="(min-width: 768px)"
-                    srcSet={product.images.tablet}
-                />
-                <img
-                    src={product.images.mobile}
-                    alt="Product"
-                    className="lg:rounded-lg"
-                />
-            </picture>
+            <div className="lg:w-1/2 w-full">
+                {(() => {
+                    const common = { alt: product.name, sizes: '100vw' }
+                    
+                    const {
+                        props: { srcSet: desktop },
+                    } = getImageProps({
+                        ...common,
+                        width: 1080,
+                        height: 1120,
+                        quality: 100,
+                        src: product.images.desktop,
+                    })
+
+                    const {
+                        props: { srcSet: tablet },
+                    } = getImageProps({
+                        ...common,
+                        width: 768,
+                        height: 800,
+                        quality: 100,
+                        src: product.images.tablet,
+                    })
+
+                    const {
+                        props: { srcSet: mobile, ...rest },
+                    } = getImageProps({
+                        ...common,
+                        width: 375,
+                        height: 390,
+                        quality: 100,
+                        src: product.images.mobile,
+                    })
+
+                    return (
+                        <picture>
+                            <source media="(min-width: 1024px)" srcSet={desktop} />
+                            <source media="(min-width: 768px)" srcSet={tablet} />
+                            <img 
+                                {...rest} 
+                                className="w-full h-auto lg:rounded-lg"
+                                alt={product.name}
+                            />
+                        </picture>
+                    )
+                })()}
+            </div>
             <article className="flex flex-col items-center justify-center gap-6 text-center lg:w-1/2 lg:items-start lg:text-left">
                 <span className="uppercase tracking-[10px] text-(--color-orange-primary)">
                     {product.new ? "new product" : ""}
