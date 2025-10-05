8ec0656028e2af9ac172dd19757681e46df81c92
IgorST <igorstojadinovic.vs@gmail.com>
2025-10-05 15:53:03 +0200

    Updated product

--- src/components/products/product-item.tsx
+++ src/components/products/product-item.tsx
@@ -19,8 +19,8 @@ export default function ProductItem({
         >
             <div className="lg:w-1/2 w-full">
                 {(() => {
-                    const common = { alt: product.name, sizes: '100vw' }
-                    
+                    const common = { alt: product.name, sizes: "100vw" };
+
                     const {
                         props: { srcSet: desktop },
                     } = getImageProps({
@@ -29,7 +29,7 @@ export default function ProductItem({
                         height: 1120,
                         quality: 100,
                         src: product.images.desktop,
-                    })
+                    });
 
                     const {
                         props: { srcSet: tablet },
@@ -39,7 +39,7 @@ export default function ProductItem({
                         height: 800,
                         quality: 100,
                         src: product.images.tablet,
-                    })
+                    });
 
                     const {
                         props: { srcSet: mobile, ...rest },
@@ -49,19 +49,25 @@ export default function ProductItem({
                         height: 390,
                         quality: 100,
                         src: product.images.mobile,
-                    })
+                    });
 
                     return (
                         <picture>
-                            <source media="(min-width: 1024px)" srcSet={desktop} />
-                            <source media="(min-width: 768px)" srcSet={tablet} />
-                            <img 
-                                {...rest} 
+                            <source
+                                media="(min-width: 1024px)"
+                                srcSet={desktop}
+                            />
+                            <source
+                                media="(min-width: 768px)"
+                                srcSet={tablet}
+                            />
+                            <img
+                                {...rest}
                                 className="w-full h-auto lg:rounded-lg"
                                 alt={product.name}
                             />
                         </picture>
-                    )
+                    );
                 })()}
             </div>
             <article className="flex flex-col items-center justify-center gap-6 text-center lg:w-1/2 lg:items-start lg:text-left">
